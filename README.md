7d4c7fe2969ff9710d490969c1169bc1c52b71b5
vasyland <vasyland@gmail.com>
2023-11-19 16:38:30 -0500

    updated db script

--- README.md
+++ README.md
@@ -4,5 +4,33 @@ Getting current prices from the yahoo finance on a schedule basis
 # Database
 MySql 8.0.28 (MySql Community Server - GPL)
 
+mysql> describe symbol_status;
++--------------------+---------------+------+-----+---------+-------+
+| Field              | Type          | Null | Key | Default | Extra |
++--------------------+---------------+------+-----+---------+-------+
+| symbol             | varchar(10)   | NO   | PRI | NULL    |       |
+| current_price      | decimal(10,4) | YES  |     | NULL    |       |
+| current_yield      | decimal(6,4)  | YES  |     | NULL    |       |
+| upper_yield        | decimal(6,4)  | YES  |     | NULL    |       |
+| lower_yield        | decimal(6,4)  | YES  |     | NULL    |       |
+| allowedtobuy_Yield | decimal(6,4)  | YES  |     | NULL    |       |
+| sell_point_yield   | decimal(6,4)  | YES  |     | NULL    |       |
+| recomended_action  | varchar(15)   | YES  |     | NULL    |       |
+| updated_on         | datetime(6)   | YES  |     | NULL    |       |
++--------------------+---------------+------+-----+---------+-------+
+9 rows in set (0.01 sec)
+
+mysql> describe watch_symbol;
++--------------------------+---------------+------+-----+---------+-------+
+| Field                    | Type          | Null | Key | Default | Extra |
++--------------------------+---------------+------+-----+---------+-------+
+| symbol                   | varchar(10)   | NO   | PRI | NULL    |       |
+| quoterly_dividend_amount | decimal(10,4) | YES  |     | NULL    |       |
+| upper_yield              | decimal(6,4)  | YES  |     | NULL    |       |
+| lower_yield              | decimal(6,4)  | YES  |     | NULL    |       |
+| updated_on               | datetime(6)   | YES  |     | NULL    |       |
++--------------------------+---------------+------+-----+---------+-------+
+5 rows in set (0.01 sec)
+
 # Install Kotlin pluging for eclipse
 https://in-kotlin.com/ide/eclipse/
--- dbscripts/symbol-tables.sql
+++ dbscripts/symbol-tables.sql
@@ -17,30 +17,11 @@ CREATE TABLE IF NOT EXISTS `horse2`.`symbol_status` (
   `symbol` VARCHAR(10) NOT NULL COMMENT 'Stock ticker. TSX with .TO',
   `current_price` DECIMAL(10,4) NULL COMMENT 'Cuurent price taken from yahoo finance',
   `current_yield` DECIMAL(6,4) NULL COMMENT 'Current yeild calculated from (quaterly_dividend_amount * 4) /current_price * 100',
+  `upper_yield` DECIMAL(6,4) NULL DEFAULT NULL COMMENT 'Upper yield' ,
+  `lower_yield` DECIMAL(6,4) NULL DEFAULT NULL COMMENT 'Lower_yield' ,
+  `allowedtobuy_yield` DECIMAL(6,4) NULL DEFAULT NULL ,
+  `sell_point_yield` DECIMAL(6,4) NULL DEFAULT NULL COMMENT 'Status of the findings: OUTDATED, NOT AVAILABLE, ERROR, ACTIVE',
   `recomended_action` VARCHAR(15) NULL COMMENT 'Possible Values: BUY, SELL, HOLD',
-  `status` VARCHAR(25) NULL COMMENT 'Status of the findings: OUTDATED, NOT AVAILABLE, ERROR, ACTIVE',
   `updated_on` DATETIME(6) NULL,
   PRIMARY KEY (`symbol`))
 ENGINE = InnoDB;
-
-ALTER TABLE `horse2`.`symbol_status` 
-ADD COLUMN `upper_yield` DATETIME(6) NULL DEFAULT NULL COMMENT 'Upper yield' AFTER `current_yield`,
-ADD COLUMN `lower_yield` DATETIME(6) NULL DEFAULT NULL COMMENT 'Lower_yield' AFTER `upper_yield`,
-ADD COLUMN `allowedToBuyYield` DATETIME(6) NULL DEFAULT NULL AFTER `lower_yield`,
-CHANGE COLUMN `status` `sell_point` DATETIME(6) NULL DEFAULT NULL COMMENT 'Status of the findings: OUTDATED, NOT AVAILABLE, ERROR, ACTIVE' AFTER `allowedToBuyYield`;
-
-ALTER TABLE `horse2`.`symbol_status` 
-CHANGE COLUMN `allowedToBuyYield` `allowedToBuy_Yield` DATETIME(6) NULL DEFAULT NULL ;
-
-ALTER TABLE `horse2`.`symbol_status` 
-CHANGE COLUMN `allowedToBuy_Yield` `allowedtobuy_Yield` DATETIME(6) NULL DEFAULT NULL ,
-CHANGE COLUMN `sell_point` `sell_point_yield` DATETIME(6) NULL DEFAULT NULL COMMENT 'Status of the findings: OUTDATED, NOT AVAILABLE, ERROR, ACTIVE' ;
-
-ALTER TABLE `horse2`.`symbol_status` 
-CHANGE COLUMN `upper_yield` `upper_yield` DECIMAL(6,4) NULL DEFAULT NULL COMMENT 'Upper yield' ,
-CHANGE COLUMN `lower_yield` `lower_yield` DECIMAL(6,4) NULL DEFAULT NULL COMMENT 'Lower_yield' ,
-CHANGE COLUMN `allowedtobuy_Yield` `allowedtobuy_Yield` DECIMAL(6,4) NULL DEFAULT NULL ,
-CHANGE COLUMN `sell_point_yield` `sell_point_yield` DECIMAL(6,4) NULL DEFAULT NULL COMMENT 'Status of the findings: OUTDATED, NOT AVAILABLE, ERROR, ACTIVE' ;
-
-
-
